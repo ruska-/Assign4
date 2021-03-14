4be5cc0f3bcc9cf79d2991d1f59f578f42f5c4ff
Ruska Beridze <rberidze@ambergroup.ge>
2021-03-14 18:10:08 +0400

    to remove compilation errors for some time, until their turn come in terms of STEPs

--- harder/script.js
+++ harder/script.js
@@ -40,7 +40,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using either the helloSpeaker's or byeSpeaker's 'speak' method.
 // See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
+for (var curName in names) {
 
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
@@ -56,7 +56,7 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (/* fill in condition here */) {
+  if (true) { //TODO
     // byeSpeaker.xxxx
   } else {
     // helloSpeaker.xxxx
